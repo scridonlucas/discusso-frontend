513b57f8da8687afa04d71baa5e1bac6faf56630
scridonlucas <lukas_scridon@yahoo.com>
2023-09-08 15:28:54 +0300

    added chakra provider to app, fixed query

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,22 @@ import App from './app/App.tsx';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import store from './app/store.ts';
 import queryClient from './app/query.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Router>
+            <App />
+          </Router>
+        </Provider>
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
