c9d794424efc5ef83ed256e6acaf1291f1b2869d
scridonlucas <lukas_scridon@yahoo.com>
2023-09-08 16:08:05 +0300

    add dark mode using chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
 
+import { ColorModeScript } from '@chakra-ui/react';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -9,13 +11,15 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import store from './app/store.ts';
 import queryClient from './app/query.ts';
+import theme from './app/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
           <Router>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
             <App />
           </Router>
         </Provider>
