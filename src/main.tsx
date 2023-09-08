8bf0b38a03ab39a1b5b6a4b6d0f018586d7b4f53
scridonlucas <lukas_scridon@yahoo.com>
2023-09-08 14:57:24 +0300

    add react query, file structure

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,19 @@ import App from './app/App.tsx';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { QueryClientProvider } from 'react-query';
+
 import store from './app/store.ts';
+import queryClient from './app/query.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
