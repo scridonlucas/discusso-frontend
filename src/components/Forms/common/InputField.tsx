6f8ca695ca95576f38841e65508368dce3540c20
scridonlucas <lukas_scridon@yahoo.com>
2023-09-11 19:59:08 +0300

    testing error system implementation

--- src/components/Forms/common/InputField.tsx
+++ src/components/Forms/common/InputField.tsx
@@ -1,4 +1,9 @@
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
 import { FormItemProp } from '../types';
 
 const InputField = ({
@@ -11,11 +16,21 @@ const InputField = ({
   errorMessage,
   handleChange,
 }: FormItemProp) => {
-  console.log(errorMessage);
+  const validateField = (value: FormItemProp['value']): boolean => {
+    if (value.length === 0) {
+      return false;
+    }
+    return true;
+  };
   return (
-    <FormControl id={id} isRequired={isRequired}>
+    <FormControl
+      id={id}
+      isRequired={isRequired}
+      isInvalid={validateField(value)}
+    >
       <FormLabel>{labelName}</FormLabel>
       <Input name={name} type={type} value={value} onChange={handleChange} />
+      {validateField(value) && <FormErrorMessage>error</FormErrorMessage>}
     </FormControl>
   );
 };
