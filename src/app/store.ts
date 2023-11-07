5b8ecf713ef3cab599c89ba58b9f78eb76c19102
scridonlucas <lukas_scridon@yahoo.com>
2023-11-08 01:33:38 +0200

    configured global store with first reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../reducers/userReducer';
 
 const store = configureStore({
-  reducer: {},
+  reducer: { user: userReducer },
 });
 
 export default store;
