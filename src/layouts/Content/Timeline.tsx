ba359f0d4de0a45fc356b40886f4bd83d6238f18
scridonlucas <lukas_scridon@yahoo.com>
2024-05-30 01:53:07 +0300

    creating a responsive timeline with discussions page

--- src/layouts/Content/Timeline.tsx
+++ src/layouts/Content/Timeline.tsx
@@ -1,5 +1,12 @@
+import { Box, Heading, Stack } from '@chakra-ui/react';
 const Timeline = () => {
-  return <>Timeline</>;
+  return (
+    <Box display={'flex'}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Heading>test</Heading>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Timeline;
