8858ad4a55245503b6f4f96e5338a3a963e090f9
scridonlucas <lukas_scridon@yahoo.com>
2024-08-18 21:20:46 +0300

    testing discussion functionality

--- src/layouts/Content/Timeline.tsx
+++ src/layouts/Content/Timeline.tsx
@@ -1,6 +1,17 @@
 import { Flex, Heading, Stack } from '@chakra-ui/react';
+import discussionService from '../../services/discussionService';
+import { useQuery } from '@tanstack/react-query';
 
 const Timeline = () => {
+  const { data } = useQuery({
+    queryKey: ['discussions'],
+    queryFn: discussionService.gatherDiscussions,
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+
+  console.log(data?.discussions);
+
   return (
     <Flex align={'center'} justify={'center'}>
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
