5d944bfb5eaa4c21c4bd9936df82c9c75da3e600
scridonlucas <lukas_scridon@yahoo.com>
2024-09-01 03:18:47 +0300

    implemented first working version, as a prototype for user feed

--- src/layouts/Content/Timeline.tsx
+++ src/layouts/Content/Timeline.tsx
@@ -4,6 +4,8 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { Button } from '@chakra-ui/react';
 import { Spinner } from '@chakra-ui/react';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 const Timeline = () => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
@@ -23,6 +25,23 @@ const Timeline = () => {
     <Flex align={'center'} justify={'center'}>
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Heading>test</Heading>
+        <InfiniteScroll
+          dataLength={data?.pages?.length || 0}
+          next={fetchNextPage}
+          hasMore={hasNextPage}
+          loader={<Spinner size="md" />}
+          endMessage={<Text>No more discussions to load</Text>}
+        >
+          {data?.pages?.map((page, i) => (
+            <Stack key={i} spacing={4}>
+              {page.discussions.map((discussion) => (
+                <Box key={discussion.id} p={5} shadow="md" borderWidth="1px">
+                  <Text fontSize="xl">{discussion.title}</Text>
+                </Box>
+              ))}
+            </Stack>
+          ))}
+        </InfiniteScroll>
       </Stack>
     </Flex>
   );
