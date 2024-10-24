fb983f3084ef33874dcea4bd8026a7b45332867f
scridonlucas <lukas_scridon@yahoo.com>
2024-10-25 00:59:31 +0300

    updated creds

--- src/features/auth/testCredentials.ts
+++ src/features/auth/testCredentials.ts
@@ -1,6 +1,6 @@
 import { LoginUser } from '../../types/userTypes';
 
 export const testCredentials: LoginUser = {
-  email: 'johndoe@example.com',
-  password: 'Test123.',
+  email: 'vistor@discusso.com',
+  password: 'Vistor123.',
 };
