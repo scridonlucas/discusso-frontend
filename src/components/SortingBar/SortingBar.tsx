954cbaea644790087ea46645bab80b57fb385573
scridonlucas <lukas_scridon@yahoo.com>
2024-11-06 21:51:19 +0200

    sort by functionality

--- src/components/SortingBar/SortingBar.tsx
+++ src/components/SortingBar/SortingBar.tsx
@@ -1,5 +1,12 @@
-import { Flex } from '@chakra-ui/react';
-import { Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
+import {
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  MenuOptionGroup,
+} from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 
 const SortingBar = ({
@@ -9,9 +16,16 @@ const SortingBar = ({
   sortCriteria: string;
   setSortCriteria: React.Dispatch<React.SetStateAction<string>>;
 }) => {
-  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortCriteria(event.target.value);
+  const handleSortChange = (criteria: string) => {
+    setSortCriteria(criteria);
   };
+
+  const sortOptions = [
+    { value: 'recent', label: 'Recent' },
+    { value: 'most_liked', label: 'Hot' },
+    { value: 'controversial', label: 'Controversial' },
+    { value: 'oldest', label: 'Oldest' },
+  ];
 
   return (
     <Flex
@@ -22,7 +36,7 @@ const SortingBar = ({
       borderBottomColor={'gray.700'}
       borderBottomWidth="1px"
       alignItems="center"
-      justifyContent="space-between"
+      justifyContent="space-between" // Distribute space evenly between SortOptions and ViewToggle
     >
       <Menu>
         <MenuButton
@@ -32,11 +46,20 @@ const SortingBar = ({
           rightIcon={<ChevronDownIcon />}
           variant={'ghost'}
         >
-          Sort by
+          {sortOptions.find((option) => option.value === sortCriteria)?.label ||
+            'Sort by'}
         </MenuButton>
-        <MenuList>
-          <MenuItem>Best</MenuItem>
-          <MenuItem>Recent</MenuItem>
+        <MenuList w="fit-content" minW="100px" maxW="150px" p={0}>
+          <MenuOptionGroup title="Sort by">
+            {sortOptions.map((option) => (
+              <MenuItem
+                key={option.value}
+                onClick={() => handleSortChange(option.value)}
+              >
+                {option.label}
+              </MenuItem>
+            ))}
+          </MenuOptionGroup>
         </MenuList>
       </Menu>
     </Flex>
